7f5f162779f15df60719f26f7e3d37030283b5f4
Silas Salema <silazdasilva20@gmail.com>
2019-12-20 10:36:38 -0300

    Vetor de Quartos para alugar

---
+++ Exercicio_Fixação_Vetor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercicio_Fixação_Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quarto[] vetor = new Quarto[10];
+            Console.Write("Quantos quartos serão alugados?");
+            int n = int.Parse(Console.ReadLine());
+            
+
+            for(int i = 1 ; i <= n ; i++)
+            {
+                Console.WriteLine("Aluguel #" + i);
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("E-mail: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vetor[quarto] = new Quarto { Nome = nome, Email = email };
+            }
+
+            Console.WriteLine("Quartos ocupados: ");
+            for ( int i = 0; i<10; i++)
+            {
+                if(vetor[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vetor[i].Nome + ", " + vetor[i].Email);
+                }
+            }
+        }
+    }
+}
---
+++ Exercicio_Fixação_Vetor/Quarto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_Fixação_Vetor
+{
+    class Quarto
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+       
+    }
+}
